3ba8407d96fe71124d99bba093b1bdc29d15c145
Juan <jujotovil@gmail.com>
2024-07-07 20:27:12 -0500

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //PROJECT EULER
+
+        /*
+
+        //1
+
+        int sumatory = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 5 == 0 || i % 3 == 0) {
+                sumatory = sumatory + i;
+            }
+        }
+        System.out.println("The sum of all the multiples of 3 or 5 below 10 are: " + sumatory); */
+
+        //2
+
+        long sumatory = 0;
+        for (long i = 0; i < 4000000; i++) {
+
+        }
+    }
+}
